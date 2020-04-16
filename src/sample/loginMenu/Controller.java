8941e1c4d8a5ab406630cefa34a42abfc2025067
Aryn2 <63594236+Aryn2@users.noreply.github.com>
2020-04-16 12:54:59 +0600

    ad

--- src/sample/loginMenu/Controller.java
+++ src/sample/loginMenu/Controller.java
@@ -32,7 +32,7 @@ public class Controller {
     void initialize() {
         SignIn.setOnAction(event -> {
             SignIn.getScene().getWindow().hide();
-            System.out.println("Hello A");
+            System.out.println("Hello AA");
         });
     }
 
